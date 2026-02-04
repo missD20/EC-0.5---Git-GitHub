d31278c60d29004e379e6761e9149463dda542bf
Dougo Kone-Sow <92309131+missD20@users.noreply.github.com>
2026-02-04 07:16:54 -0600

    Added 2 files
    Added 2 files from previous extra credit submissions

---
+++ extra-credit-1.py
@@ -0,0 +1,28 @@
+import csv 
+
+# Dictionary to store results
+stats = {}
+
+# Open and read the CSV file
+with open('friend-count.csv', newline='') as file:
+    reader = csv.DictReader(file)
+
+    print(reader.fieldnames)
+
+    for row in reader:
+        name = row['USER']
+        friends = int(row[' FRIENDCOUNT'])
+
+        first_letter = name[0].upper()
+
+        if first_letter not in stats:
+            stats[first_letter] = {'users': 0, 'friends': 0}
+
+        stats[first_letter]['users'] += 1
+        stats[first_letter]['friends'] += friends
+
+# Print results in alphabetical order
+for letter in sorted(stats.keys()):
+    users = stats[letter]['users']
+    total_friends = stats[letter]['friends']
+    print(f"{letter} - {users} users, {total_friends} total friends")
